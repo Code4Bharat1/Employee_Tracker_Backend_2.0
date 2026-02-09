0b18097fedb2190d3f3d0a3f6a688006553bff47
Danish khan <khandanish909577@gmail.com>
2026-02-09 17:17:55 +0530

    working on modules

---
+++ src/MJ/routes/module.routes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+    createModule,
+    getModules,
+    deleteModule,
+}from "../controllers/module.controller.js";
+
+import {protect} from "../middleware/auth.middleware.js";
+import {allowRoles} from "../middleware/role.middleware.js";
+
+const router = express.Router();
+
+router.post (
+    "/create",
+    protect,
+    allowRoles("MARKETING_ADMIN", "PROJECT_MANAGER"),
+    createModule
+);
+
+router.put(
+   "/update/:id",
+    protect,
+    allowRoles("MARKETING_ADMIN", "PROJECT_MANAGER"),
+    getModules
+);
+
+router.delete(
+    "/delete/:id",
+    protect,
+    allowRoles("MARKETING_ADMIN"),
+    deleteModule
+);
+
+export default router;
