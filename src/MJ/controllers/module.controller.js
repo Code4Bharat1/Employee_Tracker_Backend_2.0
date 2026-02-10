17f5dc5e5f001c271f05d2b092dcde0415c9cc47
Danish khan <khandanish909577@gmail.com>
2026-02-10 19:50:00 +0530

    new  one

--- src/MJ/controllers/module.controller.js
+++ src/MJ/controllers/module.controller.js
@@ -1,32 +1,32 @@
 import Module from "../models/module.model.js";
 
-export const createModule = async (req, res)=>{
+export const createModule = async (req, res) => {
     try {
         const module = await Module.create(req.body);
         res.status(201).json(module);
     } catch (error) {
-        res.status(500).json({message: "Module creations failed"});
+        res.status(500).json({ message: "Module creations failed" });
     }
 };
-export const getModules = async (req,res)=>{
+export const getModules = async (req, res) => {
     try {
         const module = await Module.findByIdAndUpdate(
             req.params.id,
             req.body,
-            {new: true}
+            { new: true }
         );
         res.status(200).json(module);
     } catch (error) {
-        res.status(500).json({message:"Module update failed"});
-        
+        res.status(500).json({ message: "Module update failed" });
+
     }
 };
 
-export const deleteModule = async (req,res)=>{
+export const deleteModule = async (req, res) => {
     try {
         await Module.findByIdAndUpdate(req.params.id);
-        res.status(200).json({message:"Module delated"});
+        res.status(200).json({ message: "Module delated" });
     } catch (error) {
-        res.status(500).json({message:"Module deletion failed"});
+        res.status(500).json({ message: "Module deletion failed" });
     }
 };
